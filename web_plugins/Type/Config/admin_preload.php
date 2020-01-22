fd3da40f82ea43cd412dbede49cbe9d2f2c7e983
moldwebs <moldwebs@gmail.com>
2020-01-22 15:52:46 +0200

    Update admin_preload.php

--- web_plugins/Type/Config/admin_preload.php
+++ web_plugins/Type/Config/admin_preload.php
@@ -8,6 +8,7 @@
     }
     $this->set('types', $list_types);
     
+    /*
     foreach($types as $type){
         if(!empty($type['ObjOptType']['auto_set'])){
             CakeEventManager::instance()->attach(function($event){
@@ -19,3 +20,24 @@
             }, 'Item.create', array('passData' => array('type' => $type)));
         }
     }
+    */
+
+    foreach($types as $type){
+        if(!empty($type['ObjOptType']['auto_set'])){
+            CakeEventManager::instance()->attach(function($event){
+                
+                $types = Classregistry::init('ObjOptType')->find('all');
+                foreach($types as $type){
+                    if(empty($type['ObjOptType']['auto_set'])) continue;
+                    //$type = $event->passData['type'];
+                    $save = array('model' => 'ObjItemList', 'type' => 'extra_1', 'foreign_key' => $event->data['id'], 'rel_id' => $type['ObjOptType']['id']);
+                    if(!empty($type['ObjOptType']['auto_set_expire'])) $save['expire'] = date("Y-m-d", strtotime("+{$type['ObjOptType']['auto_set_expire']} days"));
+                    Classregistry::init('ObjOptRelation')->create();
+                    Classregistry::init('ObjOptRelation')->save($save);
+    
+                }
+                
+            }, 'Item.create', array('passData' => array('type' => $type)));
+            break;
+        }
+    }    
